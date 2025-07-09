101d36263b1344faa488433f1d6f8841c2690e67
ivin-austan <a.ivinaustan@gmail.com>
2025-07-09 22:29:51 +0400

    feat: setup Node.js, Express, MongoDB, and required packages for backend

---
+++ BackEnd/controllers/userController.js
@@ -0,0 +1,52 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password, isAdmin } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("user Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+    isAdmin,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id), //check user successfully stored and used for authentication
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error Occurred");
+  }
+});
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user.id,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+module.exports = { registerUser, authUser };
---
+++ BackEnd/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
+const app = express();
+
+dotenv.config();
+connectDB();
+
+app.use(express.json()); // Built-in middleware to parse JSON
+
+const allowedOrigins = ["http://localhost:5173"];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(notFound, errorHandler);
+
+app.get("/", (req, res) => res.send("API is running"));
+
+app.listen(5000, console.log("server started on port 5000"));
