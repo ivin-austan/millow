a10d0ebd1a1663535fdc66fdc3f9db4b814492ba
ivin-austan <a.ivinaustan@gmail.com>
2025-07-14 15:45:58 +0400

    fix: reverting the changes

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const path = require("path");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const userRoutes = require("./Routes/userRoutes");
 const propertyRoutes = require("./Routes/propertyRoutes");
+
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
+
 dotenv.config();
 connectDB();
 
-app.use(express.json()); // Built-in middleware to parse JSON
+app.use(express.json());
 
 const allowedOrigins = [
   "http://localhost:5173",
@@ -37,15 +38,6 @@ app.use("/properties", propertyRoutes);
 
 app.get("/", (req, res) => res.send("API is running"));
 
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound, errorHandler);
 
-const __dirname1 = path.resolve();
-app.use(express.static(path.join(__dirname1, "dist")));
-
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname1, "dist", "index.html"))
-);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, console.log("server started on port 5000"));
