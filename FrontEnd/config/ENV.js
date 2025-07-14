d278aa0f3ac0d3cb67d68a0821f134ea00dbf7e3
ivin-austan <a.ivinaustan@gmail.com>
2025-07-14 13:13:37 +0400

    fix: updated the frontend url

--- FrontEnd/config/ENV.js
+++ FrontEnd/config/ENV.js
@@ -1,6 +1,6 @@
 export const REACT_SERVER_URL =
   window.location.origin === "http://localhost:5173"
     ? import.meta.env.VITE_REACT_SERVER_URL
-    : "https://millow-yobb.onrender.com/";
+    : "https://millow-yobb.onrender.com";
 
 export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
